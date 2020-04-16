66c2b1a684ee5777175b392c1ee0c4c0db802a40
Boris Glavic <lordpretzel@gmail.com>
2020-04-15 19:48:04 -0500

    prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -825,11 +825,11 @@ function __bobthefish_virtualenv_python_version -S -d 'Get current Python versio
         case 'Python 2*PyPy*'
             echo $pypy_glyph
         case 'Python 3*PyPy*'
-            echo -s $pypy_glyph $superscript_glyph[3]
+            echo -s $pypy_glyph 3
         case 'Python 2*'
-            echo $superscript_glyph[2]
+            echo 2
         case 'Python 3*'
-            echo $superscript_glyph[3]
+            echo 3
     end
 end
 
